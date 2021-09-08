a4a2c02f7bdb251c8e3ead4ad66347a8b386cc59
AndreyPushai <andrey.pushai@gmail.com>
2021-09-08 13:09:26 +0300

    1) Changed save file method from with open to urllib.request.urlretireve method 2) Added namedtuple Movie up to lesson task 3) Commented out all print metadata for movie titles length 4) filling parsed_data variable from raw data now more readable 5) updated all for/in loops with 2 parameters such as director/movie instead of item[0]/item[1]

--- Lesson 4/directors.py
+++ Lesson 4/directors.py
@@ -1,28 +1,25 @@
-import requests, csv
-from collections import defaultdict, Counter
+import csv
+from collections import defaultdict, Counter, namedtuple
+from urllib.request import urlretrieve
 
+# Save csv to a file
 
 url = "https://raw.githubusercontent.com/sundeepblue/movie_rating_prediction/master/movie_metadata.csv"
-
-
-def save_movie_metadata():
-
-    raw_string_data_from_response = requests.get(url).text
-
-    with open('utilization/movie_metadata.csv', 'w')  as csv_file:
-        csv_file.write(raw_string_data_from_response)
+urlretrieve(url, 'Lesson 4/movie_metadata.csv')
 
 # Task: Get the 20 highest rated directors based on their average movie IMDB ratings.
 
-# Parse the movie_metadata.csv, using csv.DictReader you get a bunch of OrderedDicts from which you only need the following k,v pairs:
+Movie = namedtuple("Movie", "year title score")
 
 parsed_data = defaultdict(list)
 
-with open('utilization/movie_metadata.csv', 'r') as csv_file:
+# Parse the movie_metadata.csv, using csv.DictReader you get a bunch of OrderedDicts
+
+with open('Lesson 4/movie_metadata.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     lines = [line for line in csv_reader]
 
-    print("Raw list with titles length:", len(lines))
+    # print("Raw list with titles length:", len(lines))
 
 # Take movies of year >= 1960
 
@@ -31,43 +28,51 @@ for line in lines:
     if int(line['title_year']) >= 1960:
 
         # Usable Data: director_name, movie_title, title_year, imdb_score
-        parsed_data[line['director_name']].append(dict({'title_year': line['title_year'], 'movie_title': line['movie_title'], 'imdb_score': line['imdb_score']}))
 
-print("Length without empty years and years < 1960:", len(parsed_data))
+        director = line['director_name']
+        movie = Movie(year=line['title_year'], title=line['movie_title'], score=line['imdb_score'])
+        parsed_data[director].append(movie)
+
+        # Instead of this exhaustive append to parsed data we can use named tuple
+
+# print("Length without empty years and years < 1960:", len(parsed_data))
 
 # Only consider directors with a minimum of 4 movies
 
-dirs_with_less_4_movies = [item[0] for item in parsed_data.items() if len(item[1]) < 4]
+dirs_with_less_4_movies = [director for director, movie in parsed_data.items() if len(movie) < 4]
 
 for item in dirs_with_less_4_movies:
     parsed_data.pop(item)
 
-print("Length of dirs with less than 4 movies:", len(parsed_data))
+# print("Length of dirs with less than 4 movies:", len(parsed_data))
 
 # Print the top 20 highest rated directors with their movies ordered desc on rating.
 
 directors_by_score = {}
 
-for item in parsed_data.items():
+for director, movies in parsed_data.items():
 
     score = 0
 
-    for movie in item[1]:
-        score += float(movie["imdb_score"])
+    for movie in movies:
+        score += float(movie.score)
 
-    directors_by_score[item[0]] = round(score / len(item[1]), 1)
+    directors_by_score[director] = round(score / len(movies), 1)
 
 top_directors = Counter(directors_by_score).most_common(20)
 
-for director in top_directors:
-    print(director[0], director[1])
+for director, score in top_directors:
+    print(director, score)
     print("------------")
-
-    length = len(parsed_data[director[0]])
+ 
+    length = len(parsed_data[director])
 
     for number in range(length):
-        print(parsed_data[director[0]][number]['title_year'], parsed_data[director[0]][number]['movie_title'], parsed_data[director[0]][number]['imdb_score'])
-
+        print(
+            parsed_data[director][number].year,
+            parsed_data[director][number].title,
+            parsed_data[director][number].score
+            )
     print("\n")
 
 # Examlpe of realizaton:
